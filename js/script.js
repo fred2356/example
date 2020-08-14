456fb21b37730da2284f9bda3978306a46f74a34
fred2356 <47901745+fred2356@users.noreply.github.com>
2020-08-14 18:47:04 +0300

    createElement
    учился добавлять элементы на странице через форму input

--- js/script.js
+++ js/script.js
@@ -1,9 +1,34 @@
 // Скрипт изменят фон div с текстом
 
+// Обявляю переменные и нахожу нужный селектор в HTML
 let button = document.querySelector('button');
 let background = document.querySelector('.text');
+let number = document.querySelector('.number');
 
+// Вешаю на кнопку обработчик событий
 button.onclick = function() {
-    console.log('Кнопка нажата!');
+    if (button.classList.contains('added')) {
+        number.textContent--;}
+        else { number.textContent++; }
+button.classList.toggle('added');
+
+    console.log(number);
     background.classList.toggle('background2');
 }
+
+// Отправка комментариев
+let commentForm = document.querySelector('.comment-form');
+let commentList = document.querySelector('.comment-list');
+let commentField = document.querySelector('.comment-field');
+
+commentForm.onsubmit = function (evt) {
+    evt.preventDefault();
+
+    let newComment = document.createElement('li');
+    newComment.classList.add('new-comment');
+    newComment.textContent = commentField.value;
+    commentList.append(newComment);
+    commentField.value = '';
+  };
+
+
