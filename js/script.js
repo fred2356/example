8e7d25ca3dd3d594e457b5546ce835633a66a33c
fred2356 <47901745+fred2356@users.noreply.github.com>
2020-08-17 12:48:14 +0300

    Добавление приоритета задачи
    чуть чуть познакомился с ветлвлениями.
    
    создаются задачи в спсики и выделяются приоритетные задачи при их указниии при добавлении

--- js/script.js
+++ js/script.js
@@ -16,16 +16,35 @@ button.classList.toggle('added');
     background.classList.toggle('background2');
 }
 
-// Отправка комментариев
+// Отправка комментариев с приоритетом
+
 let commentForm = document.querySelector('.comment-form');
 let commentList = document.querySelector('.comment-list');
 let commentField = document.querySelector('.comment-field');
+let priority = document.querySelector('.todo-priority');
+
+// Описываю как работает кнопка с приоритетом
+
+priority.onclick = function () {
+    priority.classList.toggle('is-important');
+    if (priority.classList.contains('is-important')) {
+        priority.textContent = 'Важная задача!';
+    }
+        else {
+            priority.textContent = 'Обычная задача';
+        }
+}
+
+// Попадание задачи в список и ее добавление в случае если она важная
 
 commentForm.onsubmit = function (evt) {
     evt.preventDefault();
-
+    
     let newComment = document.createElement('li');
-    newComment.classList.add('new-comment');
+    
+    if (priority.classList.contains('is-important')) {
+        newComment.classList.add('is-important');
+    }
     newComment.textContent = commentField.value;
     commentList.append(newComment);
     commentField.value = '';
